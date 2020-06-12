046ffdd494399510a1732ca2f78cd1322e0f6fb3
StahlAttila <stattilasd@gmail.com>
2020-06-12 12:18:04 +0200

    REBO-05 init exit miocroservice project

---
+++ exit-microservice/src/main/java/com/reboardify/exitmicroservice/ExitMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.reboardify.exitmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExitMicroserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExitMicroserviceApplication.class, args);
+  }
+
+}
