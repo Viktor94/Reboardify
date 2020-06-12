528530c9dfa62553edb0212f23095b433c9ee8cb
Divad001 <gyoredavid1993@gmail.com>
2020-06-12 15:33:15 +0200

    REBO-08 init exit

---
+++ exit-microservice/src/main/java/com/reboardify/exitmicroservice/controllers/ExitController.java
@@ -0,0 +1,44 @@
+package com.reboardify.exitmicroservice.controllers;
+
+import com.reboardify.exitmicroservice.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ExitController {
+
+  private Builder webClientBuilder;
+
+  @Autowired
+  public ExitController(@Qualifier("getWebClientBuilder") Builder webClientBuilder) {
+    this.webClientBuilder = webClientBuilder;
+  }
+
+  @PostMapping("/exit")
+  public ResponseEntity<?> exit(@RequestBody Employee employee) {
+    String url = System.getenv("DB_MS_URL");
+
+    ResponseEntity<?> response = webClientBuilder.build()
+        .post()
+        .uri(url + employee.getId())
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .retrieve()
+        .toBodilessEntity()
+        .onErrorResume(WebClientResponseException.class,
+            ex -> ex.getRawStatusCode() == 409 ? Mono.just(new ResponseEntity<>(HttpStatus.CONFLICT))
+                : Mono.error(ex))
+        .block();
+
+        return new ResponseEntity<>(response.getStatusCode());
+  }
+}
